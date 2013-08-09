20d6605807a0617615eaca879d30f448a2893eb2
JacoVanStaden <jaco@yakiloo.com>
2013-08-09 15:41:31 +0200

    Created Authentication Basic Services

---
+++ SingletonTheory.Services.Host/Services/Host.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceProcess;
+
+namespace SingletonTheory.Services.Host.Services
+{
+	public partial class Host : ServiceBase
+	{
+		public Host()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnStart(string[] args)
+		{
+		}
+
+		protected override void OnStop()
+		{
+		}
+	}
+}
