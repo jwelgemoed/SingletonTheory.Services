0796992bea4693a96b28551d85128eae17e0b533
JacoVanStaden <code@yakiloo.com>
2013-08-19 17:09:59 +0200

    Simplified GetRoles service method

--- SingletonTheory.Services.AuthServices.Tests/AuthServiceTests.cs
+++ SingletonTheory.Services.AuthServices.Tests/AuthServiceTests.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections.Generic;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using ServiceStack.ServiceClient.Web;
 using ServiceStack.ServiceInterface.Auth;
 using SingletonTheory.Services.AuthServices.Tests.Helpers;
 using SingletonTheory.Services.AuthServices.TransferObjects;
+using System.Collections.Generic;
 
 namespace SingletonTheory.Services.AuthServices.Tests
 {
@@ -16,7 +16,7 @@ namespace SingletonTheory.Services.AuthServices.Tests
 			// Arrange
 			JsonServiceClient client = HTTPClientHelpers.GetClient(HTTPClientHelpers.RootUrl, HTTPClientHelpers.UserName, HTTPClientHelpers.Password);
 			AuthResponse authResponse = HTTPClientHelpers.Login();
-			UserRoleRequest request = new UserRoleRequest { UserName = authResponse.UserName, SessionId = authResponse.SessionId };
+			UserRoleRequest request = new UserRoleRequest { };
 			AuthService service = new AuthService();
 
 			// Act
@@ -42,36 +42,36 @@ namespace SingletonTheory.Services.AuthServices.Tests
 			Assert.AreNotEqual(response.Roles.Count, 0);
 		}
 
-	    [Test]
-	    public void ShouldGetAllUsers()
-	    {
-            // Arrange
-            var client = HTTPClientHelpers.GetClient(HTTPClientHelpers.RootUrl, HTTPClientHelpers.AdminUserName, HTTPClientHelpers.Password);
-            AuthResponse authResponse = HTTPClientHelpers.Login();
-            UserListRequest request = new UserListRequest();
-            AuthService service = new AuthService();
+		[Test]
+		public void ShouldGetAllUsers()
+		{
+			// Arrange
+			var client = HTTPClientHelpers.GetClient(HTTPClientHelpers.RootUrl, HTTPClientHelpers.AdminUserName, HTTPClientHelpers.Password);
+			AuthResponse authResponse = HTTPClientHelpers.Login();
+			UserListRequest request = new UserListRequest();
+			AuthService service = new AuthService();
 
-            // Act
-            List<UserAuth> response = client.Get(request);
+			// Act
+			List<UserAuth> response = client.Get(request);
 
-            // Assert
-            Assert.AreNotEqual(response.Count, 0);
-	    }
+			// Assert
+			Assert.AreNotEqual(response.Count, 0);
+		}
 
-        [Test]
-        public void ShouldAddUser()
-        {
-            // Arrange
-            var client = HTTPClientHelpers.GetClient(HTTPClientHelpers.RootUrl, HTTPClientHelpers.AdminUserName, HTTPClientHelpers.Password);
-            AuthResponse authResponse = HTTPClientHelpers.Login();
-            UserRequest request = new UserRequest(){UserName = "Fred",Password = "FredPass",Role = "Admin"};
-            AuthService service = new AuthService();
+		[Test]
+		public void ShouldAddUser()
+		{
+			// Arrange
+			var client = HTTPClientHelpers.GetClient(HTTPClientHelpers.RootUrl, HTTPClientHelpers.AdminUserName, HTTPClientHelpers.Password);
+			AuthResponse authResponse = HTTPClientHelpers.Login();
+			UserRequest request = new UserRequest() { UserName = "Fred", Password = "FredPass", Role = "Admin" };
+			AuthService service = new AuthService();
 
-            // Act
-            UserAuth response = client.Post(request);
+			// Act
+			UserAuth response = client.Post(request);
 
-            // Assert
-           // Assert.AreNotEqual(response.Count, 0);
-        }
+			// Assert
+			// Assert.AreNotEqual(response.Count, 0);
+		}
 	}
 }
