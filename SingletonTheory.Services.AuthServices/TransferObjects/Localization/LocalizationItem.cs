54530843f8d95d411c9413625c9fa54a28d37423
AlmeroSteyn <almero.steyn@gmail.com>
2013-10-08 10:34:19 +0200

    Moves DTOs for localization

---
+++ SingletonTheory.Services.AuthServices/TransferObjects/Localization/LocalizationItem.cs
@@ -0,0 +1,9 @@
+﻿namespace SingletonTheory.Services.AuthServices.TransferObjects
+{
+	public class LocalizationItem
+	{
+		public string Key { get; set; }
+		public string Value { get; set; }
+		public string Description { get; set; }
+	}
+}
