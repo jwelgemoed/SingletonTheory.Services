6ea847fd22b8234af02199715bbc738975334885
JacoVanStaden <code@yakiloo.com>
2013-10-28 18:43:32 +0100

    Cleaned up UserData class

--- SingletonTheory.Services.AuthServices/Data/UserData.cs
+++ SingletonTheory.Services.AuthServices/Data/UserData.cs
@@ -2,9 +2,7 @@
 using ServiceStack.WebHost.Endpoints;
 using SingletonTheory.Services.AuthServices.Entities;
 using SingletonTheory.Services.AuthServices.Repositories;
-using System;
 using System.Collections.Generic;
-using SSAuthInterfaces = ServiceStack.ServiceInterface.Auth;
 
 namespace SingletonTheory.Services.AuthServices.Data
 {
@@ -23,11 +21,6 @@ namespace SingletonTheory.Services.AuthServices.Data
 
 		public static void CreateUsers()
 		{
-			ClearUserAuths();
-			//CreateUserAuth(0, UserName, null, Password, "en-US", new List<string> { "user" }, new List<string> { "ThePermission" });
-			//CreateUserAuth(0, AdminUserName, null, Password, "en-US", new List<string> { "admin" }, new List<string> { "ThePermission" });
-			//CreateUserAuth(0, DutchUserName, null, Password, "nl-nl", new List<string> { "admin" }, new List<string> { "ThePermission" });
-
 			ClearUsers();
 			CreateUser(0, UserName, null, Password, "en-US", new List<int> { 2 }, new List<string> { "ThePermission" });
 			CreateUser(0, AdminUserName, null, Password, "en-US", new List<int> { 1 }, new List<string> { "ThePermission" });
@@ -44,12 +37,6 @@ namespace SingletonTheory.Services.AuthServices.Data
 			repository.ClearCollection();
 		}
 
-		private static void ClearUserAuths()
-		{
-			UserAuthRepository repository = GetUserAuthRepository() as UserAuthRepository;
-			repository.Clear();
-		}
-
 		private static void CreateUser(int id, string username, string email, string password, string language, List<int> roles = null, List<string> permissions = null)
 		{
 			UserRepository repository = GetUserRepository();
@@ -64,46 +51,6 @@ namespace SingletonTheory.Services.AuthServices.Data
 			entity.Language = language;
 
 			repository.Create(entity);
-		}
-
-		private static void CreateUserAuth(int id, string username, string email, string password, string language, List<string> roles = null, List<string> permissions = null)
-		{
-			SSAuthInterfaces.IUserAuthRepository repository = GetUserAuthRepository();
-			string hash;
-			string salt;
-			new SSAuthInterfaces.SaltedHash().GetHashAndSaltString(password, out hash, out salt);
-			Dictionary<string, string> meta = new Dictionary<string, string>();
-			meta.Add("Active", true.ToString());
-			meta.Add("Language", language);
-			SSAuthInterfaces.UserAuth userAuth = new SSAuthInterfaces.UserAuth
-			{
-				Id = id,
-				DisplayName = "DisplayName",
-				//Email = email ?? "as@if{0}.com".Fmt(id),
-				UserName = username,
-				FirstName = "FirstName",
-				LastName = "LastName",
-				PasswordHash = hash,
-				Salt = salt,
-				Roles = roles,
-				Permissions = permissions,
-				Meta = meta
-			};
-
-			try
-			{
-				repository.CreateUserAuth(userAuth, password);
-			}
-			catch (Exception ex)
-			{
-				var x = ex.Message;
-			}
-		}
-
-		private static SSAuthInterfaces.IUserAuthRepository GetUserAuthRepository()
-		{
-			Container container = EndpointHost.Config.ServiceManager.Container;
-			return container.Resolve<SSAuthInterfaces.IUserAuthRepository>();
 		}
 
 		private static UserRepository GetUserRepository()
