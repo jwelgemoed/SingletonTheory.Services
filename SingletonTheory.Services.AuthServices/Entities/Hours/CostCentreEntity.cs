42e6229e75d46332f5067d581c2f5601b21056b9
Kolie <kolies@gmail.com>
2013-10-29 14:43:10 +0100

    Urenbeheer entities

---
+++ SingletonTheory.Services.AuthServices/Entities/Hours/CostCentreEntity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using ServiceStack.DataAnnotations;
+using SingletonTheory.OrmLite.Interfaces;
+
+namespace SingletonTheory.Services.AuthServices.Entities.Hours
+{
+	[Alias("KostenPlaats")]
+	public class CostCentreEntity : IIdentifiable
+	{
+		#region Fields & Properties
+
+		[AutoIncrement]
+		public long Id { get; set; }
+
+		[Required]
+		[Index(Unique = true)]
+		[StringLength(2)]
+		public string Code { get; set; }
+
+		[Alias("Omschrijving")]
+		[StringLength(30)]
+		public string Description { get; set; }
+
+		#endregion Fields & Properties
+
+		#region IIdentifiable Members
+
+		public void SetId(long id)
+		{
+			Id = id;
+		}
+
+		#endregion IIdentifiable Members
+	}
+}
---
+++ SingletonTheory.Services.AuthServices/Entities/Hours/ItemHoursEntity.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using ServiceStack.DataAnnotations;
+using SingletonTheory.OrmLite.Annotations;
+using SingletonTheory.OrmLite.Interfaces;
+
+namespace SingletonTheory.Services.AuthServices.Entities.Hours
+{
+	[Alias("ItemUren")]
+	public class ItemHoursEntity : IIdentifiable
+	{
+		#region Fields & Properties
+
+		[AutoIncrement]
+		public long Id { get; set; }
+
+		[Required]
+		[References(typeof(HourTypeEntity))]
+		public long HourTypeId { get; set; }
+
+		[Ignore()]
+		[ReferencedEntity(typeof(HourTypeEntity), "HourTypeId")]
+		public HourTypeEntity HourTypeEntity { get; set; }
+
+		[Required]
+		[References(typeof(CostCentreEntity))]
+		public long CostCentreId { get; set; }
+
+		[Ignore()]
+		[ReferencedEntity(typeof(CostCentreEntity), "CostCentreId")]
+		public CostCentreEntity CostCentreEntity { get; set; }
+
+		[Alias("OrderNummer")]
+		[Required]
+		public int OrderNumber { get; set; }
+
+		[Alias("RuimteNummer")]
+		[Required]
+		public int RoomNumber { get; set; }
+
+		[Alias("ItemNummer")]
+		[Required]
+		public int ItemNumber { get; set; }
+
+		[Alias("OuderItemNummer")]
+		[Required]
+		public int ParentItemNumber { get; set; }
+
+		[Alias("PersoonNummer")]
+		[Required]
+		public int PersonNumber { get; set; }
+
+		[Alias("Omschrijving")]
+		[StringLength(22)]
+		public string Description { get; set; }
+
+		[Alias("Datum")]
+		public DateTime Date { get; set; }
+
+		[Alias("Verwijderd")]
+		public bool Deleted { get; set; }
+		
+		#endregion Fields & Properties
+
+		#region IIdentifiable Members
+
+		public void SetId(long id)
+		{
+			Id = id;
+		}
+
+		#endregion IIdentifiable Members
+	}
+}
