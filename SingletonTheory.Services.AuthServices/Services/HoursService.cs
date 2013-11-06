422890d4d9899a620168619ef3304fdc689d8273
AlmeroSteyn <almero.steyn@gmail.com>
2013-11-06 16:09:24 +0100

    BRID-160 Hours Service skeleton

--- SingletonTheory.Services.AuthServices/Services/HoursService.cs
+++ SingletonTheory.Services.AuthServices/Services/HoursService.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ServiceStack.Common;
 using ServiceStack.ServiceInterface;
+using SingletonTheory.Services.AuthServices.Entities.Hours;
+using SingletonTheory.Services.AuthServices.Repositories.Hours;
+using SingletonTheory.Services.AuthServices.TransferObjects.Hours;
 
 namespace SingletonTheory.Services.AuthServices.Services
 {
@@ -10,9 +14,39 @@ namespace SingletonTheory.Services.AuthServices.Services
 	{
 		#region CRUD
 
+		public ItemHoursEntry Post(ItemHoursEntry request)
+		{
+			var repository = GetItemHoursRepository();
+			var returnEntity = repository.Create(TranslateToItemHoursEntity(request));
+			return TranslateToItemHoursResponse(returnEntity);
+		}
+
 		#endregion CRUD
 
 		#region Private Methods
+		private ItemHoursRepository GetItemHoursRepository()
+		{
+			var repository = base.GetResolver().TryResolve<ItemHoursRepository>();
+			if (repository == null)
+				throw new InvalidOperationException("ItemHoursRepository not defined in IoC Container");
+
+			return repository;
+		}
+
+		private ItemHoursEntry TranslateToItemHoursResponse(ItemHoursEntity entity)
+		{
+			ItemHoursEntry response = entity.TranslateTo<ItemHoursEntry>();
+
+			return response;
+		}
+
+		private ItemHoursEntity TranslateToItemHoursEntity(ItemHoursEntry request)
+		{
+			ItemHoursEntity response = request.TranslateTo<ItemHoursEntity>();
+
+			return response;
+		}
+
 		#endregion Private Methods
 	}
 }
