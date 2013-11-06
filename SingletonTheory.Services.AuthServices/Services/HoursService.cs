a386e5e4cad15fc548b80f4fb0f62f3b1c793ae9
AlmeroSteyn <almero.steyn@gmail.com>
2013-11-06 10:03:16 +0100

    BRID-160 Service skeleton

---
+++ SingletonTheory.Services.AuthServices/Services/HoursService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStack.ServiceInterface;
+
+namespace SingletonTheory.Services.AuthServices.Services
+{
+	public class HoursService : Service
+	{
+		#region CRUD
+		#endregion CRUD
+
+		#region Private Methods
+		#endregion Private Methods
+	}
+}
---
+++ SingletonTheory.Services.AuthServices/TransferObjects/Hours/RoomHoursEntries.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStack.ServiceHost;
+
+namespace SingletonTheory.Services.AuthServices.TransferObjects.Hours
+{
+	public class RoomHoursEntries:IReturn<List<RoomHoursEntry>>
+	{
+	}
+}
