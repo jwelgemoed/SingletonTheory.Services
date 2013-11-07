430edecd18daedd1e1b40f0f7d3ce55ce9d38351
Kolie <kolies@gmail.com>
2013-11-07 16:40:37 +0100

    Contact details put added

--- SingletonTheory.Services.AuthServices/Services/ContactDetailsService.cs
+++ SingletonTheory.Services.AuthServices/Services/ContactDetailsService.cs
@@ -17,12 +17,66 @@ namespace SingletonTheory.Services.AuthServices.Services
 
 		#region Contact
 
+		public Contact Get(Contact request)
+		{
+			PersonRepository personRepository = GetPersonRepository();
+			ContactRepository contactRepository = GetContactRepository();
+			EntityRepository entityRepository = GetEntityRepository();
+
+			ContactEntity contactEntity = contactRepository.Read(request.Id);
+		
+
+			List<PersonEntity> personEntities = personRepository.Read();
+
+			return CreateGetContact(contactEntity, entityRepository, personEntities);
+		}
+
+		public Contact Put(Contact request)
+		{
+			PersonRepository personRepository = GetPersonRepository();
+			ContactRepository contactRepository = GetContactRepository();
+			EntityRepository entityRepository = GetEntityRepository();
+
+			ContactEntity contactEntity = contactRepository.Read(request.Id);
+			PersonEntity personEntity = personRepository.Read(request.PersonId);
+			EntityEntity entity = entityRepository.Read(request.EntityId);
+
+
+			contactEntity.ContactTypeId = request.ContactTypeId;
+			contactEntity.Value = request.Value;
+			contactEntity.Preffered = request.Preffered;
+			contactEntity.DeletedDate = request.DeletedDate;
+			contactEntity.EntityId = request.EntityId;
+
+
+			entity.Name = request.EntityName;
+			entity.EntityTypeId = request.EntityTypeId;
+
+
+			personEntity.OccupationNameId = request.OccupationNameId;
+			personEntity.TitleId = request.TitleId;
+			personEntity.SurnamePrefix = request.SurnamePrefix;
+			personEntity.Surname = request.Surname;
+			personEntity.MaidenNamePrefix = request.MaidenNamePrefix;
+			personEntity.Nationality = request.Nationality;
+			personEntity.DateOfBirth = request.DateOfBirth;
+			personEntity.PlaceOfBirth = request.PlaceOfBirth;
+
+			contactRepository.Update(contactEntity);
+			personRepository.Update(personEntity);
+			entityRepository.Update(entity);
+
+			List<PersonEntity> personEntities = personRepository.Read();
+		
+			return CreateGetContact(contactEntity, entityRepository, personEntities);
+		}
+
 		public List<Contact> Get(Contacts request)
 		{
 			return GetContacts();
 		}
 
-		private  List<Contact> GetContacts()
+		private List<Contact> GetContacts()
 		{
 			List<Contact> contactReturnList = new List<Contact>();
 			PersonRepository personRepository = GetPersonRepository();
@@ -34,42 +88,49 @@ namespace SingletonTheory.Services.AuthServices.Services
 
 			foreach (var contactEntity in contactEntities)
 			{
-				Contact contact = new Contact
-				{
-					Id = contactEntity.Id,
-					ContactTypeId = contactEntity.ContactTypeId,
-					Value = contactEntity.Value,
-					Preffered = contactEntity.Preffered,
-					DeletedDate = contactEntity.DeletedDate,
-					EntityId = contactEntity.EntityId
-				};
-				EntityEntity entityEntity = entityRepository.Read(contactEntity.EntityId);
-				PersonEntity personEntity = null;
-				if (entityEntity != null)
-				{
-					contact.EntityName = entityEntity.Name;
-					contact.EntityTypeId = entityEntity.EntityTypeId;
-
-					personEntity = personEntities.FirstOrDefault(x => x.EntityId == entityEntity.Id);
-				}
-
-				if (personEntity != null)
-				{
-					contact.PersonId = personEntity.Id;
-					contact.OccupationNameId = personEntity.OccupationNameId;
-					contact.TitleId = personEntity.TitleId;
-					contact.SurnamePrefix = personEntity.SurnamePrefix;
-					contact.Surname = personEntity.Surname;
-					contact.MaidenNamePrefix = personEntity.MaidenNamePrefix;
-					contact.Nationality = personEntity.Nationality;
-					contact.DateOfBirth = personEntity.DateOfBirth;
-					contact.PlaceOfBirth = personEntity.PlaceOfBirth;
-				}
+				var contact = CreateGetContact(contactEntity, entityRepository, personEntities);
 
 				contactReturnList.Add(contact);
 			}
 
 			return contactReturnList;
+		}
+
+		private static Contact CreateGetContact(ContactEntity contactEntity, EntityRepository entityRepository,
+			List<PersonEntity> personEntities)
+		{
+			Contact contact = new Contact
+			{
+				Id = contactEntity.Id,
+				ContactTypeId = contactEntity.ContactTypeId,
+				Value = contactEntity.Value,
+				Preffered = contactEntity.Preffered,
+				DeletedDate = contactEntity.DeletedDate,
+				EntityId = contactEntity.EntityId
+			};
+			EntityEntity entityEntity = entityRepository.Read(contactEntity.EntityId);
+			PersonEntity personEntity = null;
+			if (entityEntity != null)
+			{
+				contact.EntityName = entityEntity.Name;
+				contact.EntityTypeId = entityEntity.EntityTypeId;
+
+				personEntity = personEntities.FirstOrDefault(x => x.EntityId == entityEntity.Id);
+			}
+
+			if (personEntity != null)
+			{
+				contact.PersonId = personEntity.Id;
+				contact.OccupationNameId = personEntity.OccupationNameId;
+				contact.TitleId = personEntity.TitleId;
+				contact.SurnamePrefix = personEntity.SurnamePrefix;
+				contact.Surname = personEntity.Surname;
+				contact.MaidenNamePrefix = personEntity.MaidenNamePrefix;
+				contact.Nationality = personEntity.Nationality;
+				contact.DateOfBirth = personEntity.DateOfBirth;
+				contact.PlaceOfBirth = personEntity.PlaceOfBirth;
+			}
+			return contact;
 		}
 
 		#endregion Contact
